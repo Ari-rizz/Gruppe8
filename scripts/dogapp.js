01652d83354d6fe40a9229c4ce2266e38fa9d2b5
osonvisen <155632335+osonvisen@users.noreply.github.com>
2024-02-27 10:57:54 +0100

    laget fetchene og valgt hva som lastes ned

--- scripts/dogapp.js
+++ scripts/dogapp.js
@@ -16,15 +16,17 @@ async function getDogs() {
 // Lager async funksjon som henter 50 brukere fra https://randomuser.me
 
 let usersApi = [];
-
+getRandomUsers();
 async function getRandomUsers() {
     try {
         const response = await fetch(
-            "https://randomuser.me/api/?results=50&nat=no&inc=name,email,picture,dob"
+            "https://randomuser.me/api/?results=50&nat=no&inc=name,location"
         );
         const data = await response.json();
         usersApi = data.results;
         setTimeout(showUsers, 500);
+    } catch (error) {
+        console.log("Kunne ikke laste inn brukerdata: " + error);
     }
 }
 
