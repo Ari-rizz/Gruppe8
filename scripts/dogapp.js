ce7d9bbe774dfbdc52345fd1e25927ca92f65761
osonvisen <155632335+osonvisen@users.noreply.github.com>
2024-02-27 10:45:04 +0100

    lager fetch for randomUser

--- scripts/dogapp.js
+++ scripts/dogapp.js
@@ -1,5 +1,5 @@
 // lager async funksjon som leser api og returnerer data fra https://dog.ceo
-let dogsApi = [];
+let dogsImages = [];
 getDogs();
 async function getDogs() {
     try {
@@ -7,12 +7,30 @@ async function getDogs() {
             "https://dog.ceo/api/breeds/image/random/50"
         );
         const data = await response.json();
-        dogsApi = data.message;
+        dogsImages = data.message;
         setTimeout(showDogs, 500);
     } catch (error) {
         console.log("Kunne ikke laste inn hundedata: " + error);
     }
 }
+// Lager async funksjon som henter 50 brukere fra https://randomuser.me
+
+let usersApi = [];
+
+async function getRandomUsers() {
+    try {
+        const response = await fetch(
+            "https://randomuser.me/api/?results=50&nat=no&inc=name,email,picture,dob"
+        );
+        const data = await response.json();
+        usersApi = data.results;
+        setTimeout(showUsers, 500);
+    }
+}
+
 function showDogs() {
-    console.log(dogsApi);
+    console.log(dogsImages);
+}
+function showUsers() {
+    console.log(usersApi);
 }
