ecfc78f2e51dc5846ee7890247c0d64fec6bc98b
Ari-rizz <arian.rizzuti@gokstadakademiet.no>
2024-02-27 09:51:28 +0100

    Lagde litt js kode

---
+++ scripts/daiting.js
@@ -0,0 +1,11 @@
+const profileCard = document.querySelector(`profile`)
+
+function swipeLeftAndRigth(event){
+
+if(evnet.key === `ArrowLeft`){
+    newProfile();
+}
+else if(event.key === `ArrowRigth`){
+    newProfile();
+}
+};
