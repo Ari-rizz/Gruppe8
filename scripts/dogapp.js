c50b85cb6720eb86d992e2b2a9e68f336126e8fd
osonvisen <155632335+osonvisen@users.noreply.github.com>
2024-02-27 09:39:22 +0100

    Ordnet med lenker til script og css. La til funksjon for å lese api

--- scripts/dogapp.js
+++ scripts/dogapp.js
@@ -1,1 +1,17 @@
-ghdadsa
+// lager async funksjon som leser api og returnerer data fra https://dog.ceo
+let dogsApi = [];
+async function getDogs() {
+    try {
+        const response = await fetch(
+            "https://dog.ceo/api/breeds/image/random/50"
+        );
+        const data = await response.json();
+        dogsApi = data.message;
+        setTimeout(showDogs, 100);
+    } catch (error) {
+        console.log("Kunne ikke laste inn hundedata: " + error);
+    }
+}
+function showDogs() {
+    console.log(dogsApi);
+}
