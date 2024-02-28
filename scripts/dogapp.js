991f82d17565fec91613023c53be937301602789
Ari-rizz <arian.rizzuti@gokstadakademiet.no>
2024-02-28 13:56:10 +0100

    brukte den gamle koden for å få inn en slette knap

--- scripts/dogapp.js
+++ scripts/dogapp.js
@@ -3,62 +3,73 @@ let usersApi = []; // Henter inn navn og bosted fra randomuser.me
 let dogs = []; // Oppretter array som skal inneholde objekter til kortene.
 let cardSection = document.querySelector(".card-section");
 
+//const breedsToFilter = ["labrador","bulldog","pitbull","golden retriver","husky"] for filtrering av hunder,valgte bare 5 tilfeldige
+
 getDogs();
 async function getDogs() {
-    try {
-        const response = await fetch(
-            "https://dog.ceo/api/breeds/image/random/50"
-        );
-        const data = await response.json();
-        dogsImages = data.message;
-        // createDogsProfileCard();
-        setTimeout(getRandomUsers, 500);
-    } catch (error) {
-        console.log("Kunne ikke laste inn hundedata: " + error);
-    }
+  try {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random/50");
+    const data = await response.json();
+    dogsImages = data.message;
+    // createDogsProfileCard();
+    setTimeout(getRandomUsers, 500);
+  } catch (error) {
+    console.log("Kunne ikke laste inn hundedata: " + error);
+  }
 }
 
 async function getRandomUsers() {
-    try {
-        const response = await fetch(
-            "https://randomuser.me/api/?results=50&nat=no&inc=name,location"
-        );
-        const data = await response.json();
-        usersApi = data.results;
-        // createDogsProfileCard();
-        setTimeout(makeDogsArray, 500);
-    } catch (error) {
-        console.log("Kunne ikke laste inn brukerdata: " + error);
-    }
+  try {
+    const response = await fetch(
+      "https://randomuser.me/api/?results=50&nat=no&inc=name,location"
+    );
+    const data = await response.json();
+    usersApi = data.results;
+    // createDogsProfileCard();
+    setTimeout(makeDogsArray, 500);
+  } catch (error) {
+    console.log("Kunne ikke laste inn brukerdata: " + error);
+  }
 }
 
 function makeDogsArray() {
-    // Lager et array som inneholder objekter med dogs. Henter bilde fra dogsImages og navn og bosted fra usersApi
-    for (let i = 0; i < dogsImages.length; i++) {
-        let dog = {
-            image: dogsImages[i],
-            name: usersApi[i].name.first, // + " " + usersApi[i].name.last, Vet ikke om vi trenger etternavnet?
-            location: usersApi[i].location.city,
-        };
-        dogs.push(dog);
-    }
-    createDogsProfileCard();
+  // Lager et array som inneholder objekter med dogs. Henter bilde fra dogsImages og navn og bosted fra usersApi
+  //    for (let i = 0; i < 10; i++)
+  for (let i = 0; i < dogsImages.length; i++) {
+    let dog = {
+      image: dogsImages[i],
+      name: usersApi[i].name.first, // + " " + usersApi[i].name.last, Vet ikke om vi trenger etternavnet?
+      location: usersApi[i].location.city,
+      // breed:fuksjon for breedfilter
+    };
+    dogs.push(dog);
+  }
+  createDogsProfileCard();
 }
 
 function createDogsProfileCard() {
-    cardSection.innerHTML = "";
-    dogs.forEach((dog, index) => {
-        const dogCard = document.createElement("article");
-        dogCard.classList.add("card");
-        dogCard.innerHTML = `<img src= "${dogs[index].image}" alt="${dogs[index].name}" style="width: 100%">`;
+  cardSection.innerHTML = "";
+  dogs.forEach((dog, index) => {
+    const dogCard = document.createElement("article");
+    dogCard.classList.add("card");
+    dogCard.innerHTML = `<img src= "${dogs[index].image}" alt="${dogs[index].name}" style="width: 100%">`;
 
-        const dogInfo = document.createElement("div");
-        dogInfo.classList.add("container");
-        dogInfo.innerHTML += `<h3>${dogs[index].name}</h3>`;
-        dogInfo.innerHTML += `<p>${dogs[index].location}</p>`;
-        dogCard.appendChild(dogInfo);
-        cardSection.appendChild(dogCard);
+    const dogInfo = document.createElement("div");
+    dogInfo.classList.add("container");
+    dogInfo.innerHTML += `<h3>${dogs[index].name}</h3>`;
+    dogInfo.innerHTML += `<p>${dogs[index].location}</p>`;
+    dogCard.appendChild(dogInfo);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "delete";
+    deleteButton.addEventListener("click", () => {
+     dogCard.remove();
     });
+   
+    dogCard.appendChild(deleteButton);
+
+    cardSection.appendChild(dogCard);
+  });
 }
 // async function createDogsProfileCard() {
 //     try {
@@ -96,4 +107,3 @@ function createDogsProfileCard() {
 // //tror denne nå skal funke -Arian
 // const newDogBtn = document.querySelector("new-dog-btn");
 // //starta på en button her men dere kan forsette
-
