19c1d13e5bf4722a2ba484576aab8a59d3b5cba7
Ari-rizz <arian.rizzuti@gokstadakademiet.no>
2024-03-04 12:07:23 +0100

    Lagd en count på show dogs og endre CDPC

--- scripts/dogapp.js
+++ scripts/dogapp.js
@@ -131,25 +131,24 @@ function makeDogsArray() {
     }
 }
 
-function createDogsProfileCard() {
+function createDogsProfileCard(displayDogs) {
     cardSection.innerHTML = "";
-    displayDogs = dogs.slice(0, 10); //blir lagt inn i nytt array
     displayDogs.forEach((dog, index) => {
         const dogCard = document.createElement("article");
         dogCard.classList.add("card");
-        dogCard.innerHTML = `<img src= "${dogs[index].image}" alt="${dogs[index].name}" style="width: 100%">`;
+        dogCard.innerHTML = `<img src= "${displayDogs[index].image}" alt="${displayDogs[index].name}" style="width: 100%">`;
 
         const dogInfo = document.createElement("div");
         dogInfo.classList.add("container");
-        dogInfo.innerHTML += `<h3>${dogs[index].name}</h3>`;
-        dogInfo.innerHTML += `<p>${dogs[index].location}</p>`;
+        dogInfo.innerHTML += `<h3>${displayDogs[index].name}</h3>`;
+        dogInfo.innerHTML += `<p>${displayDogs[index].location}</p>`;
         dogCard.appendChild(dogInfo);
 
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "Slett";
         deleteButton.addEventListener("click", () => {
             dogCard.remove();
-            dogs.splice(index, 1); //fjerner hunden fra arrayet
+            displayDogs.splice(index, 1); //fjerner hunden fra arrayet
             replaceCard();
         });
         dogCard.appendChild(deleteButton);
@@ -174,14 +173,19 @@ newDogBtn.addEventListener("click", () => {
     showDogs();
 });
 
-function showDogs() {
-    const showingDogs = 10; // antall hunder som vises
-    const displayDogs = dogs.slice(0, showingDogs); //legger hundene som er vist inn i displayDogs (tar ut og legges i nytt array)
+let showDogsCounter = 0;
 
-    if (dogs.length <= showingDogs) {
-        getDogs();
-    }
-    console.log("nye hunder", displayDogs);
+function showDogs() {
+  const showingDogs = 10; // antall hunder som vises
+  const displayDogs = dogs.slice(showDogsCounter, showDogsCounter + showingDogs); //legger hundene som er vist inn i displayDogs (tar ut og legges i nytt array)
+
+  if (dogs.length <= showingDogs) {
+      getDogs();
+  }
+
+createDogsProfileCard(displayDogs)
+showDogsCounter += showingDogs;
+  console.log("nye hunder", displayDogs);
 }
 
 //function for å erstatte det slettede elemente
