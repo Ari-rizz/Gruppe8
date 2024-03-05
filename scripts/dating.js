1cbb25c58bb55d472b9e2ac17f26b2a787de6b8f
Ari-rizz <arian.rizzuti@gokstadakademiet.no>
2024-03-05 13:42:25 +0100

    Startet litt på dating appen

--- scripts/dating.js
+++ scripts/dating.js
@@ -1,28 +1,39 @@
 let people = [];
+let profiles = 0;
 
 async function getRandomUsers() {
-    try {
-        const response = await fetch(
-            "https://randomuser.me/api/?results=50&nat=no&inc=name,location"
-        );
-        const data = await response.json();
-        people = data.results;
-        setTimeout(showUsers, 500);
-    } catch (error) {
-        console.log("Kunne ikke laste inn brukerdata: " + error);
-    }
+  try {
+    const response = await fetch(
+      "https://randomuser.me/api/?results=50&nat=no&inc=name,location"
+    );
+    const data = await response.json();
+    people = data.results;
+    setTimeout(showUsers, 500);
+  } catch (error) {
+    console.log("Kunne ikke laste inn brukerdata: " + error);
+  }
 }
 function showUsers() {
-    console.log(people);
+  createProfile();
+  console.log(profiles);
 }
-const profileCard = document.querySelector(`profile`)
+function createProfile() {
+  const profile = people;
+  const cardSection = document.querySelector(`.card-section`);
+  const profileCard = document.querySelector(`.card`);
+  cardSection.innerHTML = "";
+  profileCard.innerHTML = `
+<img src="${profile.picture.large}" alt="${profile.name.first} sitt profil bilde.">
+<h4>${profile.name.first}</h4>
+<p>${profile.location.city}, ${profile.location.country}</p>`;
+}
 
-function swipeLeftAndRigth(event){
+function swipeLeftAndRigth(event) {
+  if (event.key === `ArrowLeft`) {
+    showUsers();
+  } else if (event.key === `ArrowRigth`) {
+    showUsers();
+  }
+}
 
-if(evnet.key === `ArrowLeft`){
-    newProfile();
-}
-else if(event.key === `ArrowRigth`){
-    newProfile();
-}
-};
+
