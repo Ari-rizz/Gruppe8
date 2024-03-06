22cadcccfab2527ec7841f0b10de197b735bc541
osonvisen <155632335+osonvisen@users.noreply.github.com>
2024-03-06 18:46:59 +0100

    Laget innledende sorterings-rutiner

--- scripts/dating.js
+++ scripts/dating.js
@@ -1,5 +1,12 @@
-let people = [];
-let profiles = 0;
+let peopleApi = []; // Her er vår database med alle objekter
+let sortedPeople = []; // Her er vår database med menn eller kvinner
+let likedprofiles = []; // Her legges våre favoritter
+let likes = 10; // Antall swipes
+let preferedGender = "";
+
+womenBtn = document.querySelector("#womenBtn");
+menBtn = document.querySelector("#menBtn");
+bothBtn = document.querySelector("#bothBtn");
 
 getRandomUsers();
 async function getRandomUsers() {
@@ -9,9 +16,33 @@ async function getRandomUsers() {
         );
         const data = await response.json();
         people = data.results;
-        setTimeout(showUsers, 500);
+        setTimeout(selectGender, 500);
     } catch (error) {
         console.log("Kunne ikke laste inn brukerdata: " + error);
+    }
+}
+
+womenBtn.addEventListener("click", () => {
+    preferedGender = "female";
+
+    selectGender(preferedGender);
+});
+menBtn.addEventListener("click", () => {
+    preferedGender = "male";
+    selectGender(preferedGender);
+});
+bothBtn.addEventListener("click", () => {
+    preferedGender = "both";
+    selectGender(preferedGender);
+});
+
+function selectGender(gender) {
+    if (gender !== "both") {
+        sortedPeople = people.filter((person) => person.gender === gender);
+        console.log(sortedPeople);
+    } else {
+        sortedPeople = people;
+        console.log(sortedPeople);
     }
 }
 
