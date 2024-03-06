644debe5a08bbe29f8d37ed7fef9190aa4b9e2e0
osonvisen <155632335+osonvisen@users.noreply.github.com>
2024-03-06 12:10:49 +0100

    Etablerer variabler og arrays

--- scripts/dating.js
+++ scripts/dating.js
@@ -1,28 +1,32 @@
 let people = [];
+let likedProfiles = [];
+let likes = 10;
 
+getRandomUsers();
 async function getRandomUsers() {
     try {
         const response = await fetch(
-            "https://randomuser.me/api/?results=50&nat=no&inc=name,location"
+            "https://randomuser.me/api/?results=50&nat=no&inc=picture,gender,name,location"
         );
         const data = await response.json();
         people = data.results;
-        setTimeout(showUsers, 500);
+        setTimeout(showProfile, 500);
     } catch (error) {
         console.log("Kunne ikke laste inn brukerdata: " + error);
     }
 }
-function showUsers() {
+function showProfile() {
+    const profile = document.createElement("div");
+    profile.classList.add("profiles"); // Hvis vi skal style med css-fil
+
     console.log(people);
 }
-const profileCard = document.querySelector(`profile`)
+const profileCard = document.querySelector(`profile`);
 
-function swipeLeftAndRigth(event){
-
-if(evnet.key === `ArrowLeft`){
-    newProfile();
+function swipeLeftAndRigth(event) {
+    if (evnet.key === `ArrowLeft`) {
+        newProfile();
+    } else if (event.key === `ArrowRigth`) {
+        newProfile();
+    }
 }
-else if(event.key === `ArrowRigth`){
-    newProfile();
-}
-};
